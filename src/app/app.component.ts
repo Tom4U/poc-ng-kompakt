16d280131ef459f7e51e1f7f8f9e13a2dcf64ba2
Tom <tom@ohms.pro>
2022-10-18 13:50:11 +0200

    refactor(app): move to separate template and style files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<h1 class="default-bg">Hallo Welt!</h1>`,
-  styles: ['h1 {text-align: center; padding-top: var(--default-padding); padding-bottom: var(--default-padding);}']
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-demo';
+  title = 'Begrüßung';
 }
