3955a1b52582abc8091e520321a1091c9b7d410f
Tom <tom@ohms.pro>
2022-10-18 15:06:50 +0200

    feat(app): use ngIf

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-  title = 'Begrüßung';
+  title = "Begrüßung";
+
+  get hasGreeting(): boolean {
+    return this.greeting.length > 0;
+  }
+
+  private greeting = "";
 }
