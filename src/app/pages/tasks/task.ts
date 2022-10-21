db28863ca237c39bd4bd3165d8bd11af8aa6c6c4
Tom <tom@ohms.pro>
2022-10-21 10:21:41 +0200

    feat(tasks): change to reactive forms

--- src/app/pages/tasks/task.ts
+++ src/app/pages/tasks/task.ts
@@ -1,5 +1,15 @@
+import { FormBuilder, FormControl, FormGroup, Validators } from "@angular/forms";
+import { User } from "./user";
+
 export class Task {
     public id = 0;
 
-    constructor(public title: string) {}
+    constructor(public title: string, public user: User) {}
+
+    toFormGroup(formBuilder: FormBuilder): FormGroup {
+        return formBuilder.group({
+            title: new FormControl<string>(this.title, Validators.required),
+            user: this.user.toFormGroup(formBuilder)
+        });
+    }
 }
